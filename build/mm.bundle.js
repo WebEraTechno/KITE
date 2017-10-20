9dba5d7a5d38240c767f65cc49c0261c88cfdbab
WebEraTechno <weberahyd.tech@gmail.com>
2017-10-20 14:42:41 +0530

    url

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -65721,7 +65721,7 @@ angular.module('mm.core')
     "demo_sites": {"student": {"url": "http://school.demo.moodle.net", "username": "student", "password": "moodle"}, "teacher": {"url": "http://school.demo.moodle.net", "username": "teacher", "password": "moodle"}, "cva": {"url": "http://mm.cvaconsulting.com/moodle", "username": "student", "password": "student"}},
     "gcmpn": "694767596569",
     "customurlscheme": "moodlemobile",
-    "siteurl": "https://kite.medvarsity.com",
+    "siteurl": "https://kite.medvarsity.com/app_login",
     "skipssoconfirmation": "false",
     "forcedefaultlanguage": "false",
     "privacypolicy": "https://moodle.org/mod/page/view.php?id=8148"
